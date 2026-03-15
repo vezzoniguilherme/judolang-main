7315f7fe03a7400d8e5759263de68ed26708a337
vezzoniguilherme <vezzoni.guilherme@gmail.com>
2026-03-15 20:09:48 -0300

    Update env.ts

--- src/Constants/env.ts
+++ src/Constants/env.ts
@@ -1,2 +1,2 @@
 export const API_URL = "https://judokapro-backend.onrender.com";
-export const GOOGLE_CLIENT_ID = 477055197012-a31nelcb3lrll633ka69kuq01n6ja2e6.apps.googleusercontent.com;
+export const GOOGLE_CLIENT_ID = "477055197012-a31nelcb3lrll633ka69kuq01n6ja2e6.apps.googleusercontent.com";
