47a2d96ebd04b79dc188790073f6297012d7722d
vezzoniguilherme <vezzoni.guilherme@gmail.com>
2026-03-15 19:46:59 -0300

    Update env.ts

--- src/Constants/env.ts
+++ src/Constants/env.ts
@@ -1,1 +1,2 @@
-export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+export const API_URL = "https://judokapro-backend.onrender.com";
+export const GOOGLE_CLIENT_ID = 477055197012-a31nelcb3lrll633ka69kuq01n6ja2e6.apps.googleusercontent.com;
