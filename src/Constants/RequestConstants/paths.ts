48bdbdfd1805c04f7722da5d30848f7d05f0f27f
vezzoniguilherme <vezzoni.guilherme@gmail.com>
2026-03-15 23:55:45 -0300

    Change API_URL to an empty string

--- src/Constants/RequestConstants/paths.ts
+++ src/Constants/RequestConstants/paths.ts
@@ -1,7 +1,7 @@
 //export const API_URL = "http://localhost:";
 //export const API_PORT = "8080";
 
-export const API_URL = import.meta.env.VITE_API_URL!;
+export const API_URL = "";
 
 export const API_PREFIX = "/api";
 
